391a6b9b5ab2618b29ac0f23cfa5dac187eb3129
Toon Tamil India <121746774+ToonTamilIndia@users.noreply.github.com>
2024-08-11 09:45:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=101)
 
 def keep_alive():
     t = Thread(target=run)
